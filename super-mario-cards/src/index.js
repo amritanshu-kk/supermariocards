c46d9c0f8d39cfe6e3f0b6acb6bf981ec13b88b9
Amritanshu  Verma(The Alpha King) <amritanshu26dec@gmail.com>
2021-02-10 17:25:40 +0530

    Update index.js

--- super-mario-cards/src/index.js
+++ super-mario-cards/src/index.js
@@ -13,3 +13,12 @@ ReactDOM.render(
       <App />
   </Provider>,
   document.getElementById('root'));
+
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="/__/firebase/8.2.6/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+
+<!-- Initialize Firebase -->
+<script src="/__/firebase/init.js"></script>
